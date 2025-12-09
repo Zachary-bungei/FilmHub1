19c507fb751376d27fb41808aaff19c7c7de8284
Zachary-bungei <160045464+Zachary-bungei@users.noreply.github.com>
2025-12-09 14:26:26 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -104,7 +104,7 @@ async function doSignup(data) {
     if(result.success == true){
          errform("Created account successfully");
          setTimeout(() => {
-            window.location.replace("/assets");
+            // window.location.replace("/assets");
 
         }, 1000);
     }else{
