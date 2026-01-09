98ac078208fe8367c323e8119541201147f30222
Zachary-bungei <160045464+Zachary-bungei@users.noreply.github.com>
2026-01-09 15:54:31 +0300

    Update session.js

--- public/dashboard/session.js
+++ public/dashboard/session.js
@@ -1,1 +1,22 @@
+async function checkSession() {
+    const res = await fetch("https://filmhub-x7on.onrender.com/checksession", {
+      method: "POST",
+      credentials: "include", // 👈 sends cookies
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  
+    const data = await res.json();
+    console.log("Session check:", data);
+  
+    if (data.loggedIn) {
+      console.log("User email:", data.user.email);
+      console.log("Username:", data.user.name);
+    //   console.log("Profile image:", data.user.profile_img);
+    }
+  }
 
+  window.addEventListener("DOMContentLoaded", () => {
+    checkSession();
+  });
