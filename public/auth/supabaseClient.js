efd58e4f965780f061dec7c4a60473fb5eda2bde
Zachary-bungei <160045464+Zachary-bungei@users.noreply.github.com>
2026-01-12 16:31:05 +0300

    Update supabaseClient.js

--- public/auth/supabaseClient.js
+++ public/auth/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-SUPABASE_URL=https://djmrrtmkcoyiupmkeblh.supabase.co
-SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlseHBjbHRpeW56cXBpd3hrY3JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNTgwMjQsImV4cCI6MjA3NDkzNDAyNH0.gKWPPFpPXcN6fiAN72I11uQz714dKwk_Mr_T5ms-oy4
+let SUPABASE_URL = "https://djmrrtmkcoyiupmkeblh.supabase.co"
+let SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlseHBjbHRpeW56cXBpd3hrY3JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNTgwMjQsImV4cCI6MjA3NDkzNDAyNH0.gKWPPFpPXcN6fiAN72I11uQz714dKwk_Mr_T5ms-oy4"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
