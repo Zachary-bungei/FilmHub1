7162e7607cf81b4562cfe5236e1444afa922a82d
Zachary-bungei <160045464+Zachary-bungei@users.noreply.github.com>
2026-01-12 10:55:19 +0300

    Update main.js

--- public/auth/main.js
+++ public/auth/main.js
@@ -1,8 +1,9 @@
-const MAX_ATTEMPTS = 800; 
+const MAX_ATTEMPTS = 95; 
 let LogMode = true; 
 
 function getInputData() {
     const email = document.getElementById('SignEmail').value.trim();
+    const username1 = document.getElementById('Us1Name').value.trim();
     const password = document.getElementById('pwd1').value.trim();
     const confirmPassword = document.getElementById('pwd2').value.trim();
 
@@ -12,12 +13,12 @@ function getInputData() {
             return null;
         }
     }else{
-        if (!email || !password || !confirmPassword) {
+        if (!email || !password || !username1 || !confirmPassword) {
             errform("warning", "red", "All fields are required!");
             return null;
         }
     }
-    return { email, password, confirmPassword };
+    return { email, username1, password, confirmPassword};
 }
 
 // Validate input values
@@ -25,7 +26,7 @@ function validateInput(data) {
     // Email basic regex check
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(data.email)) {
-        errform("warning", "red", "Invalid email format!");
+        errform("warning", "red","Invalid email format!");
         return false;
     }
 
@@ -36,14 +37,13 @@ function validateInput(data) {
     }
     if(!LogMode){
         if (data.password !== data.confirmPassword) {
-            errform("warning", "red", "Passwords don't match!");
+            errform("warning", "red","Passwords do not match!");
             return false;
         }
     }
 
     return true;
 }
-
 
 // Track attempts in localStorage
 function checkAttempts() {
@@ -67,61 +67,64 @@ function checkAttempts() {
 }
 
 // Handle submit button click
-const SignBtn = document.getElementById('SignBtn');
+let SignBtn = document.getElementById('SignBtn');
 document.getElementById('SignBtn').addEventListener('click', () => {
-    SignBtn.innerText = "Submitting…";
     SignBtn.disable = true;
+    SignBtn.innerHTML = "Submitting.."
     const data = getInputData();
     if (!data){
-        SignBtn.disable = false;
-        SignBtn.innerText = "Submit";
+      SignBtn.disabled = false;
+      SignBtn.innerHTML = "Submit"
         return;
     }  // missing fields
 
     if (!validateInput(data)) {
-        SignBtn.disable = false;
-        SignBtn.innerText = "Submit";
+      SignBtn.disabled = false;
+      SignBtn.innerHTML = "Submit"
         return;
     }; // failed validation
 
     if (!checkAttempts()){
-        SignBtn.disable = false;
-        SignBtn.innerText = "Submit";
+      SignBtn.disabled = false;
+      SignBtn.innerHTML = "Submit"
         return;
     }; // exceeded attempts
 
+    // If all validations pass, send data
     // sendData(data);
-    if(LogMode == true){
-        doLogin(data);
+    if(LogMode){
+       doSignin(data);
+
     }else{
-       doSignup(data);
+      doSignup(data);
     }
+    
 });
 
 // Function to send data to main.php
 async function doSignup(data) {
     const result = await sendData("signup", {
       email: data.email,
-      username: data.username,  
+      username: data.username1,
       password: data.password
     });
-     console.log("Signup:", result);
+    console.log("Signup:", result);
     if(result.success == true){
-         errform("check_circle", "green", "Created account successfully");
-         window.location.replace("/verify");
-    }else{
-        errform("warning", "red", "Failed to create account");
-        SignBtn.disable = false;
-        SignBtn.innerText = "Submit";
-    }    
+      errform("check_circle", "green", "Created account successfully");
+      window.location.replace("/verify");
+ }else{
+     errform("warning", "red", "Failed to create account");
+     SignBtn.disable = false;
+     SignBtn.innerText = "Submit";
+ }  
   }
-async function doLogin(data) {
-    const result = await sendData("login", {
-      email: data.email, 
-      username: data.username,
+async function doSignin(data) {
+  const result = await sendData("login", {
+      email: data.email,
       password: data.password
-    });
-    console.log("Login:", result);
+  });
+
+  console.log("Login:", result);
     if(result.success == true){
          errform("check_circle", "green", " login successfully");
          window.location.replace("/dashboard");
@@ -130,15 +133,15 @@ async function doLogin(data) {
         SignBtn.disable = false;
         SignBtn.innerText = "Submit";
     } 
-  }
-async function sendData(action1, data = {}) {
-   try {
-       const response = await fetch("https://filmhub-x7on.onrender.com/auth", {
-         method: "POST",
+}
+async function sendData(action, data ={}) {
+    try {
+        const response = await fetch("https://filmhub-x7on.onrender.com/auth", {
+          method: "POST",
           headers: {
             "Content-Type": "application/json"
           },
-          body: JSON.stringify({ action: action1, data })
+          body: JSON.stringify({ action, data })
         });
     
         const result = await response.json();
@@ -148,26 +151,47 @@ async function sendData(action1, data = {}) {
         console.error("Request failed:", err);
         return { success: false, error: err.message };
       }
+      
 //       .finally(() => {
 //         document.getElementById("SignBtn").disabled = false;
 //   });
-    
 }
 
+
+
+// async function sendAction(action, data = {}) {
+//     try {
+//       const response = await fetch("https://filmhub-x7on.onrender.com/auth", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify({ action, data })
+//       });
+  
+//       const result = await response.json();
+//       return result;
+//     } catch (err) {
+//       console.error("Request failed:", err);
+//       return { success: false, error: err.message };
+//     }
+//   }
+  
 
 function errform(icon1, bgcolor, error) {
-    const ErrP = document.getElementById('ErrForm');
-    const errorbox = document.getElementById('LogMsg');
-    const ErrIocn = document.getElementById('errIocn');
-    errorbox.style.outlineColor = bgcolor;
-    errorbox.style.backgroundColor = bgcolor;
-    ErrIocn.innerText = icon1;
-    ErrP.textContent = error;
-    errorbox.style.visibility = "visible";
-    setTimeout(() => {
-        errorbox.style.visibility = "hidden";
-    }, 10000);
+  const ErrP = document.getElementById('ErrForm');
+  const errorbox = document.getElementById('LogMsg');
+  const ErrIocn = document.getElementById('errIocn');
+  errorbox.style.outlineColor = bgcolor;
+  errorbox.style.backgroundColor = bgcolor;
+  ErrIocn.innerText = icon1;
+  ErrP.textContent = error;
+  errorbox.style.visibility = "visible";
+  setTimeout(() => {
+      errorbox.style.visibility = "hidden";
+  }, 10000);
 }
+
 
 
 const slider = document.getElementById("slider");
@@ -182,6 +206,42 @@ loginBtn.addEventListener("click", () => {
 signupBtn.addEventListener("click", () => {
     slider.style.transform = "translateX(113%)";
     LogMode = false;
+});
+
+let isHidden = true; 
+const pwd1 = document.getElementById('pwd1');
+const pwd2 = document.getElementById('pwd2');
+const toggle = document.getElementById('PwdToggle');
+
+
+document.getElementById("mode-container").addEventListener('click', ()=>{
+  if(LogMode){
+    document.getElementById("checkboxTM").style.display = "none"; 
+    document.getElementById("termsLnk").style.display = "flex";
+    document.getElementById("TMagree").style.display = "none"; 
+    document.getElementById("zx_register_field1").style.display = "none";
+    document.getElementById("zx_register_field2").style.display = "none";
+  }else{
+    document.getElementById("checkboxTM").style.display = "flex";
+    document.getElementById("termsLnk").style.display = "none";
+    document.getElementById("TMagree").style.display = "flex"; 
+    document.getElementById("zx_register_field1").style.display = "flex";
+    document.getElementById("zx_register_field2").style.display = "flex";
+  }
+});
+// document.getElementById("termsLnk").style.display = "none"
+toggle.addEventListener('click', () => {
+isHidden = !isHidden; 
+if (isHidden) {
+    pwd1.type = 'password';
+    pwd2.type = 'password';
+    
+    // toggle.textContent = 'Show';
+} else {
+    pwd1.type = 'text';
+    pwd2.type = 'text';
+    // toggle.textContent = 'Hide';
+}
 });
 
 async function checkSession() {
@@ -202,4 +262,7 @@ async function checkSession() {
     //   console.log("Profile image:", data.user.profile_img);
     }
   }
-checkSession();
+
+window.addEventListener("DOMContentLoaded", () => {
+  checkSession();
+});
