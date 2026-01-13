59c6339c08914e3c4bbd40f947695ab5594d12bc
Zachary-bungei <160045464+Zachary-bungei@users.noreply.github.com>
2026-01-13 13:36:31 +0300

    Update main.js

--- public/auth/main.js
+++ public/auth/main.js
@@ -136,9 +136,9 @@ async function doSignin(data) {
         SignBtn.innerText = "Submit";
     }else{
         errform("check_circle", "green", " login successfully");
-        localStorage.setItem('access_token', result1.access_token)
-        localStorage.setItem('refresh_token', result1.refresh_token)
-        localStorage.setItem('expires_at', result1.expires_at)
+        localStorage.setItem('access_token', access_token)
+        localStorage.setItem('refresh_token', refresh_token)
+        localStorage.setItem('expires_at', expires_at)
         
         alert(data.access_token);
         const token = localStorage.getItem('access_token')
