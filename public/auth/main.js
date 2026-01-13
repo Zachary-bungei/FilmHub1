90d8729883adb5488a97b3cd721b80908933b552
Zachary-bungei <160045464+Zachary-bungei@users.noreply.github.com>
2026-01-13 11:46:54 +0300

    Update main.js

--- public/auth/main.js
+++ public/auth/main.js
@@ -257,27 +257,27 @@ if (isHidden) {
 });
 
 async function checkSession() {
-    const res = await fetch("https://filmhub-x7on.onrender.com/checksession", {
-      method: "POST",
-      credentials: "include", // 👈 sends cookies
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  
-    const data = await res.json();
-    console.log("Session check:", data);
-  
-    if (data.loggedIn) {
-      console.log("User email:", data.user.email);
-      console.log("Username:", data.user.name);
-    //   console.log("Profile image:", data.user.profile_img);
-    }
+  const token = sessionStorage.getItem("access_token");
+
+  if (!token) {
+    console.log("No session token found");
+    return;
   }
 
-window.addEventListener("DOMContentLoaded", () => {
-  checkSession();
-});
+  const res = await fetch("https://filmhub-x7on.onrender.com/checksession", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const data = await res.json();
+  // console.log("Session check:", data);
+    
+}
+window.addEventListener("DOMContentLoaded", checkSession);
+
 
 
 
